84cf2c4c12eee0d7917433a030f34a64b49e02ba
wnsgud0428 <wnsgud0428@naver.com>
2025-04-23 17:44:55 +0900

    fetch join 추가

--- src/main/java/com/example/api_test/repository/OrderRepository.java
+++ src/main/java/com/example/api_test/repository/OrderRepository.java
@@ -2,8 +2,22 @@ package com.example.api_test.repository;
 
 import com.example.api_test.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o JOIN FETCH o.person WHERE  = :orderId")
+    Optional<Order> findOrderWithPerson(@Param("orderId") Long orderId);
+
+    List<Order> findByPersonId(Long personId);
+
+    @Query("SELECT o FROM Order o JOIN FETCH o.person")
+    List<Order> findAllWithPerson();
+
+    @Query("SELECT o FROM Order o JOIN FETCH o.person p WHERE  = :personId")
+    List<Order> findByPersonIdWithPerson(@Param("personId") Long personId);
 }
